c5d0031c5d6dd0b188326a3682fd1ab2b3579595
Gulshan Kumar <gulshan311604@gmail.com>
2025-03-30 16:51:55 +0530

    work on backend and admin panel

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,10 +1,13 @@
-import mongoose from 'mongoose';
-const  connectDB = async () => {
+import mongoose from "mongoose";
 
-    mongoose.connection.on('connected', () => {
-        console.log('DB is Connected');
+const connectDB = async () => {
+
+    mongoose.connection.on('connected',() => {
+        console.log("DB Connected");
     })
 
     await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`)
+
 }
+
 export default connectDB;
--- backend/server.js
+++ backend/server.js
@@ -4,25 +4,24 @@ import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
 import userRouter from './routes/userRoute.js'
-
+import productRouter from './routes/productRoute.js'
 
 // App Config
-const app =express()
+const app = express()
 const port = process.env.PORT || 4000
 connectDB()
 connectCloudinary()
-
 
 // middlewares
 app.use(express.json())
 app.use(cors())
 
 // api endpoints
-app.use('/api/user', userRouter)
-
+app.use('/api/user',userRouter)
+app.use('/api/product',productRouter)
 
 app.get('/',(req,res)=>{
-    res.send("API Working")
+    res.send("API working")
 })
 
-app.listen(port, ()=> console.log('Server started on PORT :'+ port))
+app.listen(port,()=> console.log('Server started on PORT : '+port))
